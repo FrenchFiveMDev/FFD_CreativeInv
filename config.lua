b005501454183cfa940be530873bc90c671eb12f
FFD <106989634+FrenchFiveMDev@users.noreply.github.com>
2023-07-12 13:43:03 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+Config.NameCreativeMenu = 'Creative Chest'
+Config.NameTrashAdmin = 'Admin Trash'
+
+-- Max Weight of the Chest
+Config.MaxWeight = 500000000000000000    -- 20000 = 20kg
+Config.MaxWeightTrash = 500000000000000000    -- 20000 = 20kg
+Config.MaxSlotsCreativeMenu = 1000
+Config.MaxSlotsTrash = 1000
