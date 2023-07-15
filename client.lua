3fc5bf2562c8f02f20ab5c68e64f39bdb4a2371e
Space V <ahcene.zidahnal@epitech.eu>
2023-07-15 02:35:23 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,19 +1,5 @@
-local ESX <const> = exports['es_extended']:getSharedObject()
 local inventory <const> = exports.ox_inventory
 local player <const> = LocalPlayer.state
-
-RegisterCommand('creativechest', function()
-  ESX.TriggerServerCallback('FFDAdmin:getUsergroup', function(group)
-    if group == 'admin' or group == 'superadmin' or group == 'mod' or group == 'help' then
-      if inventory:openInventory('stash', 'admintrash') == false then
-        TriggerServerEvent('ox:creativechest')
-        inventory:openInventory('stash', 'admintrash')
-      end
-    else 
-      ESX.ShowNotification("Vous devez être admin.")
-    end
-  end)
-end, false)
 
 RegisterCommand('creativechest', function()
   if not Config.groups[player.group] then return print("Vous devez être admin.") end
